c573f64abba04df1e8352b0894d72b8c3f4730a9
hector <h.rosales01@ufromail.cl>
2026-05-12 22:01:26 -0400

    ✨ feat: add lazy-loaded guides route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,5 +2,10 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent }
+  { path: '', component: HomeComponent },
+  {
+    path: 'guides',
+    loadChildren: () =>
+      import('./guides/guides.routes').then(m => m.GUIDES_ROUTES)
+  }
 ];
