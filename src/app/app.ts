c0ffada250d94086e4d741a7c72a4cd0cff1e974
hector <h.rosales01@ufromail.cl>
2026-05-16 09:27:30 -0400

    🐛 fix: change import component of footer/footer -> guides/footer/footer

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,7 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from './navbar/navbar';
-import { Footer } from './footer/footer';
+import { Footer } from './guides/footer/footer';
 
 @Component({
   selector: 'app-root',
